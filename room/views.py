18769a5171ff0fa3f3bfc29a147e2ab7cd2f649f
WeerawitPM <63030079@kmitl.ac.th>
2024-08-07 19:46:11 +0700

    update: calendar

--- room/views.py
+++ room/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
-
+from .models import Booking
+import json
 
 # Create your views here.
 def room(request):
-    return render(request, "room/index.html")
+    bookings = Booking.objects.all()
+    events = []
+    for booking in bookings:
+        events.append(
+            {
+                "title": booking.title,
+                "start": booking.start_date.isoformat(),
+                "end": booking.end_date.isoformat(),
+                "description": booking.description,
+                "color": (
+                    booking.color.name if booking.color else "blue"
+                ),  # Use a default color if not set
+            }
+        )
+    context = {
+        "events": json.dumps(events),  # แปลง events เป็น JSON
+    }
+    return render(request, "room/index.html", context)
