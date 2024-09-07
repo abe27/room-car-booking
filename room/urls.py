996ebb9b804c37ce0cde9781341fab800f97b7cb
Weerawit Phumphuang <weerawit.pm@gmail.com>
2024-09-07 16:46:14 +0700

    update: room history page

--- room/urls.py
+++ room/urls.py
@@ -2,39 +2,11 @@ from django.urls import path, include
 from room import views
 
 urlpatterns = [
-    path("", views.room, name="room"),
     path("dashboard/", views.dashboard, name="room_dashboard"),
     path("booking/", views.booking, name="room_booking"),
     path("profile", views.profile, name="profile"),
-    path("fetch_bookings/", views.fetch_bookings, name="room_fetch_bookings"),
     path("save_booking/", views.save_booking, name="room_save_booking"),
     path("cancel_booking/", views.cancel_booking, name="room_cancel_booking"),
     path("history/", views.history, name="room_history"),
-    
-    path(
-        "approve-booking/<int:booking_id>/",
-        views.approve_booking,
-        name="room_approve_booking",
-    ),
-    path(
-        "reject-booking/<int:booking_id>/",
-        views.reject_bookings,
-        name="room_reject_booking",
-    ),
-    path(
-        "staff-cancel-booking/<int:booking_id>/",
-        views.staff_cancel_bookings,
-        name="room_staff-cancel-booking",
-    ),
-    
-    path("approve/waiting/", views.waiting, name="room_waiting"),
-    path("approve/approved/", views.approved, name="room_approved"),
-    path("approve/rejected/", views.rejected, name="room_rejected"),
     path("approve/cancel/", views.cancel, name="room_cancel"),
-    path("approve/all/", views.all_status, name="room_all_status"),
-    path("approve/all_waiting/", views.all_waiting_bookings, name="room_all_waiting"),
-    path("approve/all_approved/", views.all_approved_bookings, name="room_all_approved"),
-    path("approve/all_rejected/", views.all_rejected_bookings, name="room_all_rejected"),
-    path("approve/all_cancel/", views.all_cancel_bookings, name="room_all_cancel"),
-    path("approve/all_all/", views.all_all_status_bookings, name="room_all_all_status"),
 ]
--- room/views.py
+++ room/views.py
@@ -1,18 +1,14 @@
 from django.shortcuts import render, redirect, get_object_or_404  # type: ignore
 from django.http import JsonResponse  # type: ignore
 from .models import Room_Status, Room, Booking, Status
-from django.views.decorators.csrf import csrf_exempt  # type: ignore
 import json
 from datetime import datetime
 import requests  # type: ignore
-from django.core.serializers import serialize
 
 
 def booking(request):
+    context = {"url": "booking"}
     if request.user.is_authenticated:
-        context = {
-            "url": "booking"
-        }
         if request.method == "POST":
             date = request.POST.get("date")
             start_time = request.POST.get("start_time")
@@ -27,8 +23,10 @@ def booking(request):
                 start_date__lt=end_datetime, end_date__gt=start_datetime
             ).values_list("room_id", flat=True)
 
-            available_rooms = Room.objects.exclude(id__in=booked_rooms).order_by(
-                "sequence"
+            available_rooms = (
+                Room.objects.filter(company=request.user.fccorp)
+                .exclude(id__in=booked_rooms)
+                .order_by("sequence")
             )
 
             context.update(
@@ -42,15 +40,15 @@ def booking(request):
 
             return render(request, "room/booking/index.html", context)
         else:
-            return render(request, "room/booking/index.html")
+            return render(request, "room/booking/index.html", context)
     else:
         return redirect("/")
 
 
 def dashboard(request):
-    context = {}
+    context = {"url": "dashboard"}
     if request.user.is_authenticated:
-        rooms = Room.objects.all()
+        rooms = Room.objects.filter(company=request.user.fccorp)
         context.update({"rooms": rooms})
         if request.method == "POST":
             room_id = request.POST.get("room_id")
@@ -88,59 +86,10 @@ def dashboard(request):
             return render(request, "room/dashboard/index.html", context)
         else:
             return render(request, "room/dashboard/index.html", context)
-
-
-def profile(request):
-    if request.user.is_authenticated:
-        room_id = request.GET.get("room_id")
-        room = Room.objects.filter(id=room_id).first()
-        context = {"room": room, "url": "profile"}
-        return render(request, "room/profile/index.html", context)
     else:
         return redirect("/")
 
 
-def room(request):
-    if request.user.is_authenticated:
-        user_company = request.user.fccorp
-        room_status = Room_Status.objects.get(name="Active")
-        rooms = Room.objects.filter(company=user_company, status=room_status).order_by(
-            "sequence"
-        )
-        context = {
-            "rooms": rooms,
-            "default_room": rooms[0] if rooms.exists() else None,  # เพิ่ม default_room
-        }
-        return render(request, "room/home/index.html", context)
-    else:
-        return redirect("/")
-
-
-def fetch_bookings(request):
-    room_id = request.GET.get("room_id")
-    statuses = Status.objects.filter(name__in=["Waiting", "Approved"])
-    bookings = Booking.objects.filter(room_id=room_id, status__in=statuses)
-    booking_data = []
-    for booking in bookings:
-        booking_data.append(
-            {
-                "id": booking.pk,
-                "emp_id": booking.employee.emp_id,
-                "first_name": booking.employee.first_name,
-                "last_name": booking.employee.last_name,
-                "title": booking.title,
-                "start_date": booking.start_date.isoformat(),
-                "end_date": booking.end_date.isoformat(),
-                "description": booking.description,
-                "status": booking.status.name,
-                "color": booking.status.color,
-            }
-        )
-    # print(booking_data)
-    return JsonResponse({"bookings": json.dumps(booking_data)})
-
-
-# @csrf_exempt
 def save_booking(request):
     if request.method == "POST":
         try:
@@ -266,67 +215,26 @@ def history(request):
     if request.user.is_authenticated:
         user_id = request.user.id
         user_company = request.user.fccorp
-        room_id = request.GET.get("room_id")
-        room = Room.objects.filter(id=room_id).first()
         bookings = Booking.objects.filter(
             room__company=user_company, employee__id=user_id
         ).order_by("-created_at")
-        context = {"room": room, "bookings": bookings, "url": "history"}
+        statuses = Status.objects.all().order_by("sequence")
+        context = {
+            "bookings": bookings,
+            "url": "history",
+            "statuses": statuses,
+        }
         return render(request, "room/history/index.html", context)
     else:
         return redirect("/")
 
 
-def waiting(request):
+def profile(request):
     if request.user.is_authenticated:
-        # if request.user.fcdept.fcname == "HR" or request.user.fcdept.fcname == "IT":
-        if request.user.is_staff == True:
-            user_company = request.user.fccorp
-            room_id = request.GET.get("room_id")
-            room = Room.objects.filter(id=room_id).first()
-            bookings = Booking.objects.filter(
-                room__company=user_company, room=room, status__name="Waiting"
-            ).order_by("created_at")
-            context = {"room": room, "bookings": bookings, "url": "waiting"}
-            return render(request, "room/waiting/index.html", context)
-        else:
-            return redirect("/")
-    else:
-        return redirect("/")
-
-
-def approved(request):
-    if request.user.is_authenticated:
-        # if request.user.fcdept.fcname == "HR" or request.user.fcdept.fcname == "IT":
-        if request.user.is_staff == True:
-            user_company = request.user.fccorp
-            room_id = request.GET.get("room_id")
-            room = Room.objects.filter(id=room_id).first()
-            bookings = Booking.objects.filter(
-                room__company=user_company, room=room, status__name="Approved"
-            ).order_by("-created_at")
-            context = {"room": room, "bookings": bookings, "url": "approved"}
-            return render(request, "room/approve/index.html", context)
-        else:
-            return redirect("/")
-    else:
-        return redirect("/")
-
-
-def rejected(request):
-    if request.user.is_authenticated:
-        # if request.user.fcdept.fcname == "HR" or request.user.fcdept.fcname == "IT":
-        if request.user.is_staff == True:
-            user_company = request.user.fccorp
-            room_id = request.GET.get("room_id")
-            room = Room.objects.filter(id=room_id).first()
-            bookings = Booking.objects.filter(
-                room__company=user_company, room=room, status__name="Rejected"
-            ).order_by("-created_at")
-            context = {"room": room, "bookings": bookings, "url": "rejected"}
-            return render(request, "room/approve/index.html", context)
-        else:
-            return redirect("/")
+        room_id = request.GET.get("room_id")
+        room = Room.objects.filter(id=room_id).first()
+        context = {"room": room, "url": "profile"}
+        return render(request, "room/profile/index.html", context)
     else:
         return redirect("/")
 
@@ -342,298 +250,6 @@ def cancel(request):
                 room__company=user_company, room=room, status__name="Canceled"
             ).order_by("-created_at")
             context = {"room": room, "bookings": bookings, "url": "cancel"}
-            return render(request, "room/approve/index.html", context)
-        else:
-            return redirect("/")
-    else:
-        return redirect("/")
-
-
-def all_status(request):
-    if request.user.is_authenticated:
-        # if request.user.fcdept.fcname == "HR" or request.user.fcdept.fcname == "IT":
-        if request.user.is_staff == True:
-            user_company = request.user.fccorp
-            room_id = request.GET.get("room_id")
-            room = Room.objects.filter(id=room_id).first()
-            bookings = Booking.objects.filter(
-                room__company=user_company, room=room
-            ).order_by("-created_at")
-            context = {"room": room, "bookings": bookings, "url": "all_status"}
-            return render(request, "room/approve/index.html", context)
-        else:
-            return redirect("/")
-    else:
-        return redirect("/")
-
-
-def approve_booking(request, booking_id):
-    # booking = Booking.objects.get(id=booking_id)
-    booking = get_object_or_404(Booking, id=booking_id)
-
-    # Set the status to Approved
-    approved_status = get_object_or_404(Status, name="Approved")
-    booking.status = approved_status
-
-    # Assign the approver as the current logged-in user
-    booking.approver = request.user
-
-    # Parse the JSON body to get the remark
-    data = json.loads(request.body)
-    remark = data.get("remark", "").strip()
-
-    booking.remark = remark
-    booking.save()
-
-    # Reject other bookings that overlap with the approved booking
-    overlapping_bookings = Booking.objects.filter(
-        room=booking.room,
-        status__name="Waiting",
-        start_date__lt=booking.end_date,
-        end_date__gt=booking.start_date,
-    ).exclude(id=booking.id)
-
-    rejected_status = get_object_or_404(Status, name="Rejected")
-
-    for other_booking in overlapping_bookings:
-        other_booking.status = rejected_status
-        other_booking.approver = request.user
-        other_booking.remark = "มีคนจองห้องนี้แล้ว"
-        other_booking.save()
-
-    # Send Line Notify
-    line_notify_token = request.user.fccorp.line_notify_room
-    line_notify_url = "https://notify-api.line.me/api/notify"
-    headers = {"Authorization": f"Bearer {line_notify_token}"}
-    message = (
-        f"\nBooking ID: {booking.id}\n"
-        f"Room: {booking.room.name}\n"
-        f"Title: {booking.title}\n"
-        f"Requester: {booking.employee.first_name} {booking.employee.last_name}\n"
-        f"Description: {booking.description}\n"
-        f"Tel: {booking.employee.tel}\n"
-        f"Start: {booking.start_date.strftime('%d/%m/%Y %H:%M')}\n"
-        f"End: {booking.end_date.strftime('%d/%m/%Y %H:%M')}\n"
-        f"Remark: {booking.remark}\n"
-        f"Status: {booking.status.name}"
-    )
-    payload = {"message": message}
-
-    response = requests.post(line_notify_url, headers=headers, data=payload)
-    if response.status_code != 200:
-        return JsonResponse(
-            {
-                "status": "Booking approved successfully!, but failed to send Line notification."
-            },
-            status=200,
-        )
-    else:
-        return JsonResponse(
-            {
-                "status": "Booking approved successfully and Line notification sent successfully!"
-            },
-            status=200,
-        )
-
-
-def reject_bookings(request, booking_id):
-    if request.method == "POST":
-        booking = get_object_or_404(Booking, id=booking_id)
-
-        # Get the status for Rejected
-        rejected_status = get_object_or_404(Status, name="Rejected")
-        booking.status = rejected_status
-
-        # Assign the approver as the current logged-in user
-        booking.approver = request.user
-
-        # Parse the JSON body to get the remark
-        data = json.loads(request.body)
-        remark = data.get("remark", "").strip()
-
-        if not remark:
-            return JsonResponse({"status": "Remark cannot be empty!"}, status=400)
-
-        booking.remark = remark
-        booking.save()
-
-        # Send Line Notify
-        line_notify_token = request.user.fccorp.line_notify_room
-        line_notify_url = "https://notify-api.line.me/api/notify"
-        headers = {"Authorization": f"Bearer {line_notify_token}"}
-        message = (
-            f"\nBooking ID: {booking.id}\n"
-            f"Room: {booking.room.name}\n"
-            f"Title: {booking.title}\n"
-            f"Requester: {booking.employee.first_name} {booking.employee.last_name}\n"
-            f"Description: {booking.description}\n"
-            f"Tel: {booking.employee.tel}\n"
-            f"Start: {booking.start_date.strftime('%d/%m/%Y %H:%M')}\n"
-            f"End: {booking.end_date.strftime('%d/%m/%Y %H:%M')}\n"
-            f"Remark: {booking.remark}\n"
-            f"Status: {booking.status.name}"
-        )
-        payload = {"message": message}
-
-        response = requests.post(line_notify_url, headers=headers, data=payload)
-        if response.status_code != 200:
-            return JsonResponse(
-                {
-                    "status": "Booking rejected successfully!, but failed to send Line notification."
-                },
-                status=200,
-            )
-        else:
-            return JsonResponse(
-                {
-                    "status": "Booking rejected successfully and Line notification sent successfully!"
-                },
-                status=200,
-            )
-
-    return JsonResponse({"status": "Invalid request method."}, status=405)
-
-
-def staff_cancel_bookings(request, booking_id):
-    if request.method == "POST":
-        booking = get_object_or_404(Booking, id=booking_id)
-
-        # Get the status for Rejected
-        canceled_status = get_object_or_404(Status, name="Canceled")
-        booking.status = canceled_status
-
-        # Assign the approver as the current logged-in user
-        booking.approver = request.user
-
-        # Parse the JSON body to get the remark
-        data = json.loads(request.body)
-        remark = data.get("remark", "").strip()
-
-        if not remark:
-            return JsonResponse({"status": "Remark cannot be empty!"}, status=400)
-
-        booking.remark = remark
-        booking.save()
-        # Send Line Notify
-        line_notify_token = request.user.fccorp.line_notify_room
-        line_notify_url = "https://notify-api.line.me/api/notify"
-        headers = {"Authorization": f"Bearer {line_notify_token}"}
-        message = (
-            f"\nBooking ID: {booking.id}\n"
-            f"Room: {booking.room.name}\n"
-            f"Title: {booking.title}\n"
-            f"Requester: {booking.employee.first_name} {booking.employee.last_name}\n"
-            f"Description: {booking.description}\n"
-            f"Tel: {booking.employee.tel}\n"
-            f"Start: {booking.start_date.strftime('%d/%m/%Y %H:%M')}\n"
-            f"End: {booking.end_date.strftime('%d/%m/%Y %H:%M')}\n"
-            f"Remark: {booking.remark}\n"
-            f"Status: {booking.status.name}"
-        )
-        payload = {"message": message}
-
-        response = requests.post(line_notify_url, headers=headers, data=payload)
-        if response.status_code != 200:
-            return JsonResponse(
-                {
-                    "status": "Booking canceled successfully!, but failed to send Line notification."
-                },
-                status=200,
-            )
-        else:
-            return JsonResponse(
-                {
-                    "status": "Booking canceled successfully and Line notification sent successfully!"
-                },
-                status=200,
-            )
-
-    return JsonResponse({"status": "Invalid request method."}, status=405)
-
-
-def all_waiting_bookings(request):
-    if request.user.is_authenticated:
-        # if request.user.fcdept.fcname == "HR" or request.user.fcdept.fcname == "IT":
-        if request.user.is_staff == True:
-            user_company = request.user.fccorp
-            room_id = request.GET.get("room_id")
-            room = Room.objects.filter(id=room_id).first()
-            bookings = Booking.objects.filter(
-                room__company=user_company, status__name="Waiting"
-            ).order_by("created_at")
-            context = {"room": room, "bookings": bookings, "url": "all_waiting"}
-            return render(request, "room/waiting/index.html", context)
-        else:
-            return redirect("/")
-    else:
-        return redirect("/")
-
-
-def all_approved_bookings(request):
-    if request.user.is_authenticated:
-        # if request.user.fcdept.fcname == "HR" or request.user.fcdept.fcname == "IT":
-        if request.user.is_staff == True:
-            user_company = request.user.fccorp
-            room_id = request.GET.get("room_id")
-            room = Room.objects.filter(id=room_id).first()
-            bookings = Booking.objects.filter(
-                room__company=user_company, status__name="Approved"
-            ).order_by("-created_at")
-            context = {"room": room, "bookings": bookings, "url": "all_approved"}
-            return render(request, "room/approve/index.html", context)
-        else:
-            return redirect("/")
-    else:
-        return redirect("/")
-
-
-def all_rejected_bookings(request):
-    if request.user.is_authenticated:
-        # if request.user.fcdept.fcname == "HR" or request.user.fcdept.fcname == "IT":
-        if request.user.is_staff == True:
-            user_company = request.user.fccorp
-            room_id = request.GET.get("room_id")
-            room = Room.objects.filter(id=room_id).first()
-            bookings = Booking.objects.filter(
-                room__company=user_company, status__name="Rejected"
-            ).order_by("-created_at")
-            context = {"room": room, "bookings": bookings, "url": "all_rejected"}
-            return render(request, "room/approve/index.html", context)
-        else:
-            return redirect("/")
-    else:
-        return redirect("/")
-
-
-def all_cancel_bookings(request):
-    if request.user.is_authenticated:
-        # if request.user.fcdept.fcname == "HR" or request.user.fcdept.fcname == "IT":
-        if request.user.is_staff == True:
-            user_company = request.user.fccorp
-            room_id = request.GET.get("room_id")
-            room = Room.objects.filter(id=room_id).first()
-            bookings = Booking.objects.filter(
-                room__company=user_company, status__name="Canceled"
-            ).order_by("-created_at")
-            context = {"room": room, "bookings": bookings, "url": "all_cancel"}
-            return render(request, "room/approve/index.html", context)
-        else:
-            return redirect("/")
-    else:
-        return redirect("/")
-
-
-def all_all_status_bookings(request):
-    if request.user.is_authenticated:
-        # if request.user.fcdept.fcname == "HR" or request.user.fcdept.fcname == "IT":
-        if request.user.is_staff == True:
-            user_company = request.user.fccorp
-            room_id = request.GET.get("room_id")
-            room = Room.objects.filter(id=room_id).first()
-            bookings = Booking.objects.filter(room__company=user_company).order_by(
-                "-created_at"
-            )
-            context = {"room": room, "bookings": bookings, "url": "all_all_status"}
             return render(request, "room/approve/index.html", context)
         else:
             return redirect("/")
