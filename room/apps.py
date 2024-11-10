ecc5ce66ff0fe5f87e49267c029c61ac76e2a41d
WeerawitPM <63030079@kmitl.ac.th>
2024-11-10 13:53:49 +0700

    update: fix size card in booking page

--- room/apps.py
+++ room/apps.py
@@ -5,9 +5,9 @@ class RoomConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "room"
     
-    def ready(self):
-        if os.environ.get('RUN_MAIN', None) != 'true':
-            # This ensures the code inside runs only in the main process
-            print('ready...')
-            from room import tasks
-            tasks.start()
+    # def ready(self):
+    #     if os.environ.get('RUN_MAIN', None) != 'true':
+    #         # This ensures the code inside runs only in the main process
+    #         print('ready...')
+    #         from room import tasks
+    #         tasks.start()
