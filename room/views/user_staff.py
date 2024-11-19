77baf950ba02f93b6ee7a93365712bc20ab593b8
Weerawit Phumphuang <weerawit.pm@gmail.com>
2024-11-19 09:39:35 +0700

    update: edit, delete user in staff

--- room/views/user_staff.py
+++ room/views/user_staff.py
@@ -3,6 +3,7 @@ from user.models import Employee
 from django.contrib import messages  # type: ignore
 from django.http import JsonResponse
 from room.forms import UserForm
+from company_department.models import Department
 
 
 def index(request):
@@ -17,6 +18,7 @@ def index(request):
 
     if request.user.is_authenticated and request.user.is_staff:
         users = Employee.objects.filter(fccorp=request.user.fccorp)
+        departments = Department.objects.filter(fccorp=request.user.fccorp)
         if request.method == "POST":
             form = UserForm(
                 request.POST, user=request.user
@@ -30,7 +32,8 @@ def index(request):
         context.update(
             {
                 "users": users,
-                "form": form
+                "form": form,
+                "departments": departments,
             }
         )
         return render(request, "room/staff/user/index.html", context)
@@ -57,3 +60,38 @@ def update_status(request):
                 {"success": False, "message": "ไม่พบผู้ใช้งานที่ระบุ"}, status=404
             )
     return JsonResponse({"success": False, "message": "การร้องขอไม่ถูกต้อง"}, status=400)
+
+def edit_user(request):
+    if request.method == "POST":
+        user_id = request.POST.get("user_id")
+        user = get_object_or_404(Employee, id=user_id)
+
+        # อัปเดตข้อมูลทั่วไป
+        user.emp_id = request.POST.get("emp_id")
+        user.email = request.POST.get("email")
+        user.username = request.POST.get("username")
+        user.first_name = request.POST.get("first_name")
+        user.last_name = request.POST.get("last_name")
+        user.tel = request.POST.get("tel")
+        user.fcdept_id = request.POST.get("department")
+        user.save()
+        messages.success(request, "อัพเดทผู้ใช้สำเร็จ!")  # เพิ่มข้อความแจ้งเตือน
+
+        return redirect("user_staff")
+    
+def delete_user(request):
+    if (
+        request.method == "POST"
+        and request.user.is_authenticated
+        and request.user.is_staff
+    ):
+        user_id = request.POST.get("user_id")
+        try:
+            user = Employee.objects.get(id=user_id)
+            user.delete()
+            return JsonResponse({"success": True})
+        except Employee.DoesNotExist:
+            return JsonResponse(
+                {"success": False, "message": "ไม่พบผู้ใช้หรือไม่มีสิทธิ์ในการลบ"}
+            )
+    return JsonResponse({"success": False, "message": "คำขอลบไม่ถูกต้อง"})
