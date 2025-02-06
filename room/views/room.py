7e942a9b80800f121b0214d617ab3c8bbb86d10b
Weerawit Phumphuang <weerawit.pm@gmail.com>
2025-02-06 13:19:33 +0700

    update: check authen in list card room

--- room/views/room.py
+++ room/views/room.py
@@ -1,24 +1,30 @@
 from room.models import Booking
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils import timezone
 
 def list(request):
-    today = timezone.now().date()  # Get today's date
-    bookings = Booking.objects.filter(
-        room__company=request.user.fccorp,
-        status__sequence__in=[1, 4],
-        start_date__date__gte=today  # Filter bookings starting today or later
-        # start_date__date=today  # Filter bookings starting today
-    ).order_by("start_date")[:15]
-    context = {"bookings": bookings}
-    return render(request, "room/room/list/index.html", context)
+    if request.user.is_authenticated:
+        today = timezone.now().date()  # Get today's date
+        bookings = Booking.objects.filter(
+            room__company=request.user.fccorp,
+            status__sequence__in=[1, 4],
+            start_date__date__gte=today  # Filter bookings starting today or later
+            # start_date__date=today  # Filter bookings starting today
+        ).order_by("start_date")[:15]
+        context = {"bookings": bookings}
+        return render(request, "room/room/list/index.html", context)
+    else:
+        return redirect("/")
 
 def card(request):
-    today = timezone.now().date()  # Get today's date
-    bookings = Booking.objects.filter(
-        room__company=request.user.fccorp,
-        status__sequence__in=[1, 4],
-        start_date__date=today  # Filter bookings starting today
-    ).order_by("start_date")[:15]
-    context = {"bookings": bookings}
-    return render(request, "room/room/card/index.html", context)
+    if request.user.is_authenticated:
+        today = timezone.now().date()  # Get today's date
+        bookings = Booking.objects.filter(
+            room__company=request.user.fccorp,
+            status__sequence__in=[1, 4],
+            start_date__date=today  # Filter bookings starting today
+        ).order_by("start_date")[:15]
+        context = {"bookings": bookings}
+        return render(request, "room/room/card/index.html", context)
+    else:
+        return redirect("/")
