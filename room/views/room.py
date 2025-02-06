0de6c5ba96842d0c22bcb6e772dcf761d8d6f668
Weerawit Phumphuang <weerawit.pm@gmail.com>
2025-02-06 16:13:42 +0700

    update: card

--- room/views/room.py
+++ room/views/room.py
@@ -27,10 +27,26 @@ def card(request):
                 status__sequence__in=[1, 4],
                 start_date__date=today,  # Filter bookings starting today
             )
-            .distinct("room")
-            .order_by("room", "start_date")
+            .order_by("start_date", "end_date")[:15]
         )
         context = {"bookings": bookings}
         return render(request, "room/room/card/index.html", context)
     else:
         return redirect("/")
+    
+# def card(request):
+#     if request.user.is_authenticated:
+#         today = timezone.now().date()  # Get today's date
+#         bookings = (
+#             Booking.objects.filter(
+#                 room__company=request.user.fccorp,
+#                 status__sequence__in=[1, 4],
+#                 start_date__date=today,  # Filter bookings starting today
+#             )
+#             .distinct("room")
+#             .order_by("room", "start_date")
+#         )
+#         context = {"bookings": bookings}
+#         return render(request, "room/room/card/index.html", context)
+#     else:
+#         return redirect("/")
