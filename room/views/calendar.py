526987eb85e6f345779e8868b11fc82de0c37e57
Weerawit Phumphuang <weerawit.pm@gmail.com>
2025-02-11 13:43:38 +0700

    update: add calendar to show room company

--- room/views/calendar.py
+++ room/views/calendar.py
@@ -10,16 +10,47 @@ from django.utils import timezone
 def index(request):
     companies = Company.objects.all()
     company_id = request.GET.get("company")
+    context = {}
 
     if company_id:
         rooms = Room.objects.filter(company_id=company_id)
+        bookings = Booking.objects.filter(
+            employee__fccorp_id=company_id,
+            status__sequence__in=[1, 4, 5],
+        )
+        # print(list(bookings))
+        bookings_data = []
+        for booking in bookings:
+            bookings_data.append(
+                {
+                    "id": booking.pk,
+                    "emp_id": booking.employee.emp_id,
+                    "first_name": booking.employee.first_name,
+                    "last_name": booking.employee.last_name,
+                    "title": booking.title,
+                    "start_date": booking.start_date.isoformat(),
+                    "end_date": booking.end_date.isoformat(),
+                    "description": booking.description.replace("\r\n", "<br>").replace(
+                        "\n", "<br>"
+                    ),  # แปลง \r\n และ \n เป็น <br>
+                    "status": booking.status.name,
+                    "color": booking.status.color,
+                    "remark": booking.remark.replace("\r\n", "<br>").replace(
+                        "\n", "<br>"
+                    ),  # แปลง \r\n และ \n เป็น <br>,
+                }
+            )
+
+        context = {"bookings": json.dumps(bookings_data)}
     else:
         rooms = ""
 
-    context = {
-        "companies": companies,
-        "rooms": rooms,
-    }
+    context.update(
+        {
+            "companies": companies,
+            "rooms": rooms,
+        }
+    )
     return render(request, "room/calendar/index.html", context)
 
 
